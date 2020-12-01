b6fcfd7856f4ba9212365a34ab14ba4b2b6dfa24
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-12-01 10:58:10 -0300

    log timestamp

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,4 +1,5 @@
 import { ErrorRequestHandler } from 'express';
+import { format } from 'date-fns';
 import AppError from '../errors/AppError';
 
 const errorHandler: ErrorRequestHandler = (
@@ -7,16 +8,17 @@ const errorHandler: ErrorRequestHandler = (
   response,
   _,
 ) => {
-  console.log(err);
+  console.log({ timestamp: format(Date.now(), 'dd/MM/yyyy hh:mm:ss'), err });
 
   if (err instanceof AppError)
     return response
       .status(err.statusCode)
       .json({ status: 'error', message: err.message });
 
-  return response
-    .status(500)
-    .json({ status: 'error', message: 'Internal server error' });
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
 };
 
 export default errorHandler;
