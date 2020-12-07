cc94da3aa44b7a8b8df6243c7f5c4132ce51e2f0
Rodrigo de Sousa Pacheco <39929399+rodrigopacheco100@users.noreply.github.com>
2020-12-07 15:44:29 -0300

    create user tests

---
+++ backend/src/modules/users/services/CreateUser.spec.ts
@@ -0,0 +1,43 @@
+import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '../providers/HashProvider/fakes/HashProvider';
+import FakeUserRepository from '../repositories/fakes/User';
+import CreateUserService from './CreateUser';
+
+describe('CreateUser', () => {
+  it('should be able to create an user', async () => {
+    const userRepository = new FakeUserRepository();
+    const hashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(userRepository, hashProvider);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+    expect(user.name).toBe('John Doe');
+    expect(user.email).toBe('johndoe@gmail.com');
+    expect(user).toHaveProperty('password');
+  });
+
+  it('should not be able to create an user with same email', async () => {
+    const userRepository = new FakeUserRepository();
+    const hashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(userRepository, hashProvider);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
