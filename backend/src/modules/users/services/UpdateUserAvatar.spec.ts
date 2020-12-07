bafa26a6aea894b97432a75212a445e7bb557de2
Rodrigo de Sousa Pacheco <39929399+rodrigopacheco100@users.noreply.github.com>
2020-12-07 17:02:40 -0300

    update user avatar tests

---
+++ backend/src/modules/users/services/UpdateUserAvatar.spec.ts
@@ -0,0 +1,50 @@
+import FakeStorageProvider from '@shared/container/providers/Storage/fakes/FakeStorage';
+import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '../providers/Hash/fakes/HashProvider';
+import FakeUserRepository from '../repositories/fakes/User';
+import UpdateUserAvatarService from './UpdateUserAvatar';
+
+describe('UpdateUserAvatar', () => {
+  it('should be able to update user avatar', async () => {
+    const userRepository = new FakeUserRepository();
+    const storageProvider = new FakeStorageProvider();
+    const updateUserAvatar = new UpdateUserAvatarService(
+      userRepository,
+      storageProvider,
+    );
+
+    const user = await userRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar2.jpg',
+    });
+
+    expect(user.avatar).toBe('avatar2.jpg');
+  });
+
+  it('should be able to create an user', async () => {
+    const userRepository = new FakeUserRepository();
+    const storageProvider = new FakeStorageProvider();
+    const updateUserAvatar = new UpdateUserAvatarService(
+      userRepository,
+      storageProvider,
+    );
+
+    expect(
+      updateUserAvatar.execute({
+        user_id: 'lllllll',
+        avatarFilename: 'avatar.jpg',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
