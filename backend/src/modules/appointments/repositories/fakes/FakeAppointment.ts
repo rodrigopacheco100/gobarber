23554f5b1cc199264bef9b4c820621fe860921b1
Rodrigo de Sousa Pacheco <39929399+rodrigopacheco100@users.noreply.github.com>
2020-12-07 10:32:03 -0300

    fake appointment repository

---
+++ backend/src/modules/appointments/repositories/fakes/FakeAppointment.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from 'uuid';
+import IAppointmentRepository from '@modules/appointments/repositories/IAppointment';
+
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointment';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+export default class FakeAppointmentRepository
+  implements IAppointmentRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => date === appointment.date,
+    );
+
+    return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
