1a7d81281e53ee63e35349e84522933e421c8800
Rodrigo de Sousa Pacheco <39929399+rodrigopacheco100@users.noreply.github.com>
2020-12-14 10:34:50 -0300

    fake usertoken repository

---
+++ backend/src/modules/users/repositories/fakes/UserToken.ts
@@ -0,0 +1,22 @@
+import { v4 as uuid } from 'uuid';
+
+import IUserTokenRepository from '@modules/users/repositories/IUserToken';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+export default class FakeUserTokenRepository implements IUserTokenRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken | undefined> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+}
