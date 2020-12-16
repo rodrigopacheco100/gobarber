ac4110548b86f411b48bf6d4c0f304b2453b8916
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-12-16 18:57:04 -0300

    status bar, routes, signin, signup and logo

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import { View } from 'react-native';
+import 'react-native-gesture-handler';
 
-const App: React.FC = () => {
-  return <View />;
-};
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import AuthRoutes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <AuthRoutes />
+    </View>
+  </NavigationContainer>
+);
 
 export default App;
